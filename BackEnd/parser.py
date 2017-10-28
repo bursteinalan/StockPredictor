92e526b70eba7872c576793db0b0b069e7ecb18f
Omar Sagga <osagga@bu.edu>
2017-10-28 14:24:11 -0400

    Added data parser

--- BackEnd/parser.py
+++ BackEnd/parser.py
@@ -1,14 +1,33 @@
 from flask import Flask
-from yahoo_finance import Share
-import json
+from yahoo_historical import Fetcher
+from datetime import datetime
 
+def parseTime(timeString):
+    '''
+    Converts date to total milliseconds
+    Input: Y-m-d
+    '''
+    InputDate = int(datetime.strptime(timeString, '%Y-%m-%d').strftime('%s'))
+    return InputDate*1000
 
-yahoo = Share('GOOG')
+app = Flask(__name__)
 
-print yahoo.get_price()
+@app.route('/getDataSet')
+def getDataSet(stockTicker):
+    startYear = 1980
+    while(startYear < 2017):
+        # Change the fixed value here
+        try:
+            currentDate = [int(strNum) for strNum in datetime.now().strftime("%Y-%m-%d").split('-')]
+            data = Fetcher(stockTicker, [startYear,1,1], currentDate)
+            break;
+        except ValueError:
+            startYear += 5
 
-# app = Flask(__name__)
-#
-# @app.route('/getDataSet')
-# def getDataSet(stockTicker):
-#
+    parsedData = list()
+    for _, row in data.getHistorical().iterrows():
+        try:
+            parsedData.append((parseTime(row['Date']), float(row['Close'])))
+        except ValueError:
+            continue;
+    return parsedData

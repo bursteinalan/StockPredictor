25e32d73cd2247b5b0c93c3e212dcfaee2639cf6
tc-yu <33182836+tc-yu@users.noreply.github.com>
2017-10-29 01:13:45 -0400

    removed excess print statement of commit 1

--- BackEnd/get_mapper.py
+++ BackEnd/get_mapper.py
@@ -7,9 +7,9 @@ def main(code):
     response = urllib.request.urlopen('http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=' + code + '&region=1&lang=en&callback=YAHOO.Finance.SymbolSuggest.ssCallback')
     html = response.read()
     text = html.decode()    
-    print(text)
+
     symbol = findall('"symbol":"[A-Z]*', text)
-    
+   
     return symbol[0][10:]
         
 main('alphabet')
